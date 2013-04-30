ff572d7ef46fae4c50d82c516e7e919cd50f5a32
siddhartha <siddhartha047@gmail.com>
2013-04-30 14:34:12 +0600

    created a project for working on speech

---
+++ src/com/sid/speech/SpeechSpliterActivity.java
@@ -0,0 +1,13 @@
+package com.sid.speech;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SpeechSpliterActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
